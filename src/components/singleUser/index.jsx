cc38a411b1fd04f8b727700aac35f6fb4b18e8c9
Faraaz2633 <mdfaraz4343@gmail.com>
2024-02-11 14:33:30 +0530

    [Modification]: Components modified

---
+++ src/components/singleUser/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// mui
+import { Box, Container } from '@mui/material'
+
+export default function SingleUser() {
+  return (
+    <Container maxWidth='xl'>
+        Single User
+    </Container>
+  )
+}
---
+++ src/components/users/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// mui
+import { Box, Container } from '@mui/material'
+
+export default function Users() {
+  return (
+    <Container maxWidth='xl'>
+        <Box>
+            asdf
+        </Box>
+    </Container>
+  )
+}
